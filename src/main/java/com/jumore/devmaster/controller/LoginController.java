ab4e756e3f354ba4984c8de8b77d5f80e91704a5
xzye <runningship@gmail.com>
2017-05-17 15:02:14 +0800

    bug修改

--- src/main/java/com/jumore/devmaster/controller/LoginController.java
+++ src/main/java/com/jumore/devmaster/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.jumore.devmaster.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jumore.devmaster.common.enums.BaseExceptionEnum;
 import com.jumore.devmaster.common.util.RSAKeyUtil;
 import com.jumore.dove.aop.annotation.PublicMethod;
@@ -18,6 +19,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "")
@@ -37,6 +41,26 @@ public class LoginController {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("redirect:/login");
         return mv;
+    }
+    
+    @RequestMapping(value = "/getPublicKey")
+    @ResponseBody
+    public ResponseVo<String> getPublicKey(HttpServletRequest request) throws Exception {
+        /*********   加密登录  begin *********************/
+        HashMap<String, Object> map = RSAUtils.getKeys();
+        //生成公钥和私钥
+        RSAPublicKey publicKey = (RSAPublicKey) map.get(RSAUtils.RAS_Key_Public);
+        RSAPrivateKey privateKey = (RSAPrivateKey) map.get(RSAUtils.RAS_Key_Private);
+
+        request.getSession().setAttribute(RSAUtils.RAS_Key_Private, privateKey);//私钥保存在session中，用于解密
+
+        //公钥信息保存在页面，用于加密
+        String publicKeyExponent = publicKey.getPublicExponent().toString(16);
+        String publicKeyModulus = publicKey.getModulus().toString(16);
+        JSONObject data = new JSONObject();
+        data.put("publicKeyExponent", publicKeyExponent);
+        data.put("publicKeyModulus", publicKeyModulus);
+        return ResponseVo.<String> BUILDER().setData(data.toJSONString()).setCode(Const.BUSINESS_CODE.SUCCESS);
     }
 
     @RequestMapping(value = "/doLogin")
